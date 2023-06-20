7a45d43b438b65c47b13cfa06ad087ef3945750f
YeongGi <yeonggilol@gmail.com>
2023-06-20 23:32:29 +0900

    feat: add more infos at room card

--- src/components/lobby/Room.tsx
+++ src/components/lobby/Room.tsx
@@ -1,9 +1,20 @@
 import styled from "@emotion/styled";
 
 export default function Room({ id }: { id: string }) {
+  const isPlaying = false;
+  const maxPeople = 6;
+  const currentPeople = 2;
+
   return (
     <Container>
       <Title>{id}</Title>
+      <PlayingContainer>
+        <IsPlaying isPlaying={isPlaying} />
+        {isPlaying ? "playing" : "waiting"}
+      </PlayingContainer>
+      <People>
+        {currentPeople} / {maxPeople}
+      </People>
     </Container>
   );
 }
@@ -11,7 +22,7 @@ export default function Room({ id }: { id: string }) {
 const Container = styled.div`
   width: 10rem;
   height: 10rem;
-  border-radius: 1rem;
+  position: relative;
   background-color: #ffffff30;
   display: flex;
   align-items: center;
@@ -23,9 +34,36 @@ const Container = styled.div`
   }
 `;
 
-const Title = styled.h2`
-  font-size: 2rem;
+const Title = styled.h4`
+  font-size: 1.6rem;
   color: #ffffff;
   font-weight: 400;
   text-align: center;
+`;
+
+const People = styled.p`
+  position: absolute;
+  bottom: 0.4rem;
+  right: 1.4rem;
+  font-size: 1.2rem;
+  color: #ffffff;
+  font-weight: 400;
+`;
+
+const PlayingContainer = styled.div`
+  position: absolute;
+  top: 0.8rem;
+  left: 0.8rem;
+  display: flex;
+  gap: 1rem;
+  color: #919191;
+`;
+
+const IsPlaying = styled.div<{ isPlaying: boolean }>`
+  top: 1.4rem;
+  left: 0.8rem;
+  width: 1.2rem;
+  height: 1.2rem;
+  border-radius: 50%;
+  background-color: ${({ isPlaying }) => (isPlaying ? "#8a1414" : "#2c9904")};
 `;
