1f81cc2125b820d4c3177bae7ee9013a671ffc49
Seojunhwan <seojunhwan@kakao.com>
2023-06-25 20:53:38 +0900

    refactor: isCaptain, current team

--- src/recoil/selectors/userSelector.ts
+++ src/recoil/selectors/userSelector.ts
@@ -1,14 +1,24 @@
 import { selector } from "recoil";
 import { userIdState, userNameState } from "../atoms";
+import { roomState } from "../atoms/roomState";
 
 export const userSelector = selector({
   key: "userSelector",
   get: ({ get }) => {
     const id = get(userIdState);
     const name = get(userNameState);
+    const room = get(roomState);
+
+    const isCaptain = room?.captainID === id;
+    const team = room?.teamA.player.find((player) => player.id === id)
+      ? "A"
+      : "B";
+
     return {
       id,
       name,
+      isCaptain,
+      team,
     };
   },
 });
